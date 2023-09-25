6461be7cc3c58af8fccff614b1a571cd52d5e7ce
zkh <1348996916@qq.com>
2023-09-25 11:26:08 +0800

    second commit

---
+++ C++/binary_search/binary_search.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int binary_search(const std::vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1; // 如果找不到目标元素，返回-1
+}
