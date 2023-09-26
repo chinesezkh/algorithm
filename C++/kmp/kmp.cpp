89809a7d1522a270695cfeba31053baac1f99463
zkh <1348996916@qq.com>
2023-09-26 16:51:01 +0800

    kmp_next

--- C++/kmp/kmp.cpp
+++ C++/kmp/kmp.cpp
@@ -1,6 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int strStr_next(string s, string p) {
+    int n = s.size(), m = p.size();
+    if(m == 0) return 0;
+    //设置哨兵
+    s.insert(s.begin(),' ');
+    p.insert(p.begin(),' ');
+    vector<int> next(m + 1);
+    //预处理next数组
+    for(int i = 2, j = 0; i <= m; i++){
+        while(j and p[i] != p[j + 1]) j = next[j];
+        if(p[i] == p[j + 1]) j++;
+        next[i] = j;
+    }
+    //for(int i = 1; i <= m; i++) cout << next[i] << endl;
+    //匹配过程
+    for(int i = 1, j = 0; i <= n; i++){
+        while(j and s[i] != p[j + 1]) j = next[j];
+        if(s[i] == p[j + 1]) j++;
+        if(j == m) return i - m;
+    }
+    return -1;
+}
+
+//输出s串中所有t的位置
+int strStr_next1(string s, string p,vector<int> &next) {
+    int n = s.size(), m = p.size();
+    if(m == 0) return 0;
+    //设置哨兵
+    s.insert(s.begin(),' ');
+    p.insert(p.begin(),' ');
+    next.resize(m + 1);
+    //预处理next数组
+    for(int i = 2, j = 0; i <= m; i++){
+        while(j and p[i] != p[j + 1]) j = next[j];
+        if(p[i] == p[j + 1]) j++;
+        next[i] = j;
+    }
+    //匹配过程
+    for(int i = 1, j = 0; i <= n; i++){
+        while(j and s[i] != p[j + 1]) j = next[j];
+        if(s[i] == p[j + 1]) j++;
+        if(j == m) cout << i - m + 1 << endl;
+    }
+    for(int i = 1; i <= m; i++) cout << next[i] << " ";
+    cout << endl;
+    return 0;
+}
+
+
 int strStr(string s, string p) {
     int n = s.size(), m = p.size();
     for(int i = 0; i <= n - m; i++){
@@ -17,6 +66,6 @@ int strStr(string s, string p) {
 int main(){
     string s,t;
     cin >> s >> t;
-    cout << strStr(s,t) << endl;
+    cout << strStr_next(s,t) << endl;
     return 0;
 }
